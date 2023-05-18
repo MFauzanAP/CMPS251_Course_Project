8e9f92fcca7280ae06f62fa2cb808b9ede4297bc
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2023-05-18 11:33:33 +0300

    Import code from collaborator

--- src/models/Patient.java
+++ src/models/Patient.java
@@ -1,5 +1,35 @@
 package models;
 
-public class Patient {
+public class Patient extends Slot {
+	private int qid;
+	private String name;
+	private ResidencyType residency;
+	public Patient(int qid, String name, ResidencyType residency) {
+		super();
+		this.qid = qid;
+		this.name = name;
+		this.residency = residency;
+	}
+	public int getQid() {
+		return qid;
+	}
+	public void setQid(int qid) {
+		this.qid = qid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ResidencyType getResidency() {
+		return residency;
+	}
+	public void setResidency(ResidencyType residency) {
+		this.residency = residency;
+	}
 	
+	
+	
+
 }
